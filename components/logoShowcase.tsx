a74027f6045134a3b6bb765eecb72b14d962d2ea
Bilal <jcvntx@gmail.com>
2023-10-03 00:59:45 +0300

    -u stable

--- components/logoShowcase.tsx
+++ components/logoShowcase.tsx
@@ -1,30 +1,29 @@
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import Image from "next/image";
+import { getPlaiceholder } from "plaiceholder";
+import fs from "node:fs/promises";
+import { useEffect, useState } from "react";
+import { LogoShowcaseArray } from "@/utils/LogoShowcaseArray";
 
-const LogoShowcaseArray = [
-  "/fontShowcase/9834504_167.jpg",
-  "/fontShowcase/9950635_946.jpg",
-  "/fontShowcase/11827540_4839823.jpg",
-  "/fontShowcase/17351160_BWI870.jpg",
-  "/fontShowcase/21145832_9.jpg",
-  "/fontShowcase/banana.jpg",
-];
-
-export default function LogoShowcase() {
-    return (
-      <div className="container grid grid-cols-2 md:grid-cols-3 gap-4"> 
-        {LogoShowcaseArray.map((src, index) => (
-          <div key={index}>
-            <AspectRatio ratio={1 / 1}>
-              <Image 
-                src={src}
-                alt={"Logo"}
-                fill
-                className={index % 2 === 0 ? 'col-span-2' : 'col-span-2 row-start-2'} 
-              />
-            </AspectRatio>
-          </div>
-        ))}
-      </div>
-    )
-  }
+export default async function LogoShowcase() {
+  return (
+    <div className="container grid grid-cols-2 md:grid-cols-3 gap-4">
+      {LogoShowcaseArray.map((src, index) => (
+        <div key={index}>
+          <AspectRatio ratio={1 / 1}>
+            <Image
+              src={src}
+              sizes="(max-width: 768px) 100vw, (max-width: 500px) 50vw, 33vw"
+              alt={"Logo"}
+              fill
+              placeholder="blur"
+              className={
+                index % 2 === 0 ? "col-span-2" : "col-span-2 row-start-2"
+              }
+            />
+          </AspectRatio>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ utils/LogoShowcaseArray.ts
@@ -0,0 +1,15 @@
+import monkey from "../public/fontShowcase/9834504_167.jpg";
+import banana from "../public/fontShowcase/banana.jpg";
+import dwadwa from "../public/fontShowcase/9950635_946.jpg";
+import dwadwxa from "../public/fontShowcase/11827540_4839823.jpg";
+import dwxadwxa from "../public/fontShowcase/17351160_BWI870.jpg";
+import dwadwxxa from "../public/fontShowcase/21145832_9.jpg";
+
+export const LogoShowcaseArray = [
+    banana,
+    monkey,
+    dwadwa,
+    dwadwxa,
+    dwxadwxa,
+    dwadwxxa
+  ];
