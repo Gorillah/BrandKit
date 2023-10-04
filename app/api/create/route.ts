c4544d78f606815dfdfc6f17050c8e6d9d379364
Bilal <jcvntx@gmail.com>
2023-10-04 15:47:48 +0300

    stable before update nextjs

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -10,8 +10,7 @@ export async function POST(request: NextRequest) {
   //   return NextResponse.json({ error: "unauthorized" }, { status: 401 });
   // }
   const body = await request.json();
-  const logo_description = await generateLogoPrompt(body);
-  console.log({ logo_description });
+
   const logoStyle = body.logoStyle.map((style: string) => {
     return style.split("/").pop()?.split(".").slice(-2, -1).join(".");
   });
@@ -23,18 +22,16 @@ export async function POST(request: NextRequest) {
   const fontStyle = body.fontStyle.map((font: string) => {
     return font.split("/").pop()?.split(".").slice(-2, -1).join(".");
   });
+  
+  const formData: formData = {
+    company: body.company,
+    logoStyle,
+    logoColor,
+    fontStyle
+  }
+  
+  const logo_description = await generateLogoPrompt(formData);
+  console.log({ logo_description });
+  
   return NextResponse.json(logo_description);
-
-
-  // const res = await generateImage(
-  //   body.company,
-  //   logoStyle,
-  //   logoColor,
-  //   fontStyle
-  // );
-
-  // return NextResponse.json(
-  //   { error: "An error occurred while processing the request." },
-  //   { status: 500 }
-  // );
 }
