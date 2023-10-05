61ad6ca0f7a742476c4b79e4fca7284fd9771829
Bilal <jcvntx@gmail.com>
2023-10-05 10:13:30 +0300

    s

--- app/logo/page.tsx
+++ app/logo/page.tsx
@@ -4,6 +4,7 @@ import { logos } from "@/drizzle/schema";
 import { and, eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 import Logos from "@/components/logos";
+import { Plus } from "lucide-react";
 
 export default async function Logo() {
   const { userId } = auth();
@@ -13,7 +14,10 @@ export default async function Logo() {
     eq(logos.userId, userId) // Created by current user
   );
   return (
-    <div className="container grid grid-cols-2 gap-2 my-4">
+    <div className="container grid grid-cols-2 md:grid-cols-3 gap-2 my-4">
+      <div className="bg-gray-300 flex items-center justify-center">
+        <Plus size={80} />
+      </div>
       {logoSelect.map((logo) => (
         <Logos key={logo.id} logoUrl={logo.logoUrl} id={logo.id} />
       ))}
