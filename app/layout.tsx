628287b154161e2d4d1af786774f18fc0ad0964c
Bilal <jcvntx@gmail.com>
2023-10-14 16:30:53 +0300

    modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import Provider from "@/components/Provider";
-import Script from "next/script";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const font = Roboto({
   subsets: ["latin"],
@@ -18,38 +17,22 @@ export const metadata: Metadata = {
   description: "Generate your perfect logo simply with BrandKit",
 };
 
-const GTM_ID = "AW-717418224";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <html>
-        <script>
-          {`
-            (window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'AW-717418224')
-          `}
-        </script>
-        <body>...</body>
+    <ClerkProvider>
+      <html lang="en">
+        <Provider>
+          <body className={cn(font.className)}>
+            {children}
+            {/* <ReactQueryDevtools /> */}
+            <Toaster />
+          </body>
+        </Provider>
       </html>
-      <ClerkProvider>
-        <html lang="en">
-          <Provider>
-            <body className={cn(font.className)}>
-              {children}
-              {/* <ReactQueryDevtools /> */}
-              <Toaster />
-            </body>
-          </Provider>
-        </html>
-      </ClerkProvider>
-    </div>
+    </ClerkProvider>
   );
 }
